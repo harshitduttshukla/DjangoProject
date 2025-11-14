a251e5f91a0e3acd7c806b4079eefb7769d9ff5a
Harshit Shukla <harshitshukla6388@gmail.com>
2025-11-14 12:53:30 +0530

    fix the error

--- Djangopro/harshit/admin.py
+++ Djangopro/harshit/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity, ChaiReview, Store, ChaiCertification
 
 # Register your models here.
-admin.site.register(ChaiVarity)
+
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarityAdmin(admin.ModelAdmin):
+    list_display = ('name',type,'date_added')
+    inlines = [ChaiReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('chai_varities',  )
+
+class ChaiCertificationAmin(admin.ModelAdmin):
+    list_display = ('chai','certification_number')
+
+
+admin.site.register(ChaiVarity,ChaiVarityAdmin)
+admin.site.register(Store,StoreAdmin)
+admin.site.register(ChaiCertification,ChaiCertificationAmin)
+
